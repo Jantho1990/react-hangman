3d3dedad8953be2e77fd9d1ca514ee12728c473a
Joshua Anthony <jantho1990@gmail.com>
2019-07-12 13:52:38 -0500

    WordDisplay now scales font size based on how many letters are in the word and whether user is viewing at mobile or desktop size

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const LetterSpace = styled.span`
+const LetterSpace = styled.span.attrs(props => ({
+  fontSize: `calc(50vw / ${props.wordLength})`,
+  fontSizeMobile: `calc(90vw / ${props.wordLength})`
+}))`
   color: black;
   font-size: 2rem;
+  font-size: ${({ fontSizeMobile }) => fontSizeMobile};
   margin: 0 0rem;
   width: 2rem;
+  width: ${({ fontSizeMobile }) => fontSizeMobile};
   display: inline-block;
   text-transform: capitalize;
   user-select: none;
   &.not-guessed {
     color: hsl(0, 65%, 60%);
+  }
+  @media screen and (min-width: 768px) {
+    font-size: ${({ fontSize }) => fontSize};
+    width: ${({ fontSize }) => fontSize};
   }
 `
 
@@ -30,17 +39,19 @@ export default class WordDisplay extends Component {
   ct = 0
 
   renderLetterSpace(letter) {
-    const { guessedLetters, gameOver } = this.props
+    const { guessedLetters, gameOver, word } = this.props
+
+    const wordLength = word.length
     
     if (guessedLetters.find(guessedLetter => guessedLetter === letter) === undefined) {
       if (gameOver) {
-        return <LetterSpace className="not-guessed" key={`key-letterspace-${this.ct++}`}>{letter}</LetterSpace>
+        return <LetterSpace wordLength={wordLength} className="not-guessed" key={`key-letterspace-${this.ct++}`}>{letter}</LetterSpace>
       } else {
-        return <LetterSpace key={`key-letterspace-${this.ct++}`}>&#95;</LetterSpace>
+        return <LetterSpace wordLength={wordLength} key={`key-letterspace-${this.ct++}`}>&#95;</LetterSpace>
       }
     }
 
-    return <LetterSpace key={`key-letterspace-${this.ct++}`}>{letter}</LetterSpace>
+    return <LetterSpace wordLength={wordLength} key={`key-letterspace-${this.ct++}`}>{letter}</LetterSpace>
   }
 
   renderLetterSpaces(word) {
