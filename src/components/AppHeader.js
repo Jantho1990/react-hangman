3a6510134d9f18faea374f87a24b1e9219fd9c4d
Joshua Anthony <jantho1990@gmail.com>
2019-07-22 14:48:23 -0500

    added slide-in transition for header on GameScreen

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,9 @@
+import React from 'react'
 import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
 
-export default styled.header`
-  grid-area: header;
+const AppHeaderWrapper = styled.header`
+  height: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -11,3 +13,20 @@ export default styled.header`
   color: ${props => props.theme.gameScreen.headerFontColor};
   flex: 1;
 `
+
+export default function AppHeader({children, theme}) {
+  // Slide in header and footer when entering GameScreen
+  const springHeader = useSpring({
+    from: { transform: 'translate3d(0, -100%, 0)' },
+    to: { transform: 'translate3d(0, 0%, 0)' },
+    delay: 550
+  })
+
+  return (
+    <animated.div style={{...springHeader, gridArea: 'header'}}>
+      <AppHeaderWrapper theme={theme}>
+        {children}
+      </AppHeaderWrapper>
+    </animated.div>
+  )
+}
--- src/components/screens/GameScreen.js
+++ src/components/screens/GameScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { useTransition, animated } from 'react-spring'
+import { useSpring, useTransition, animated } from 'react-spring'
 import useGameState from '../../game-state/useGameState'
 import LetterSelector from '../../components/LetterSelector'
 import WordDisplay from '../../components/word-display/WordDisplay'
