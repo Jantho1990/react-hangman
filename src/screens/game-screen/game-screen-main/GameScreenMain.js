f6df5dd1c1efd1a68ca91e86ce5cec10c70565dd
Josh Anthony <Joshua.Anthony@bestbuy.com>
2019-08-21 15:49:38 -0500

    more gamescreen stuff; added vscode settings to gitignore

--- src/screens/game-screen/game-screen-main/GameScreenMain.js
+++ src/screens/game-screen/game-screen-main/GameScreenMain.js
@@ -8,9 +8,9 @@ export default styled.div`
   grid-template-rows: 1fr 3fr 3fr;
   grid-template-rows: 10vh 45vh 45vh;
   grid-template-areas:
-    "header"
-    "center"
-    "footer";
+    'header'
+    'center'
+    'footer';
   background-color: ${({ theme }) => theme.gameScreen.backgroundColor};
   color: ${({ theme }) => theme.gameScreen.fontColor};
   @media screen and (min-width: 568px) {
@@ -19,16 +19,16 @@ export default styled.div`
     grid-template-rows: 1fr 4fr;
     grid-template-rows: 20vh 80vh;
     grid-template-areas:
-      "header header"
-      "center footer";
+      'header header'
+      'center footer';
   }
   @media screen and (min-width: 768px) {
     grid-template-columns: auto;
     grid-template-rows: 1fr 3fr 3fr;
     grid-template-rows: 10vh 45vh 45vh;
     grid-template-areas:
-      "header"
-      "center"
-      "footer";
+      'header'
+      'center'
+      'footer';
   }
 `
