fda5004f71b840099f2b481c08aaf08dfe97f356
Joshua Anthony <jantho1990@gmail.com>
2019-09-23 20:55:04 -0500

    added list of guessed letters to GameStats

--- src/game-stats/GameStats.js
+++ src/game-stats/GameStats.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import useGameState from 'game-state/useGameState'
+import LetterSpace from 'word-display/LetterSpace'
 
 const GameStatsWrapper = styled.div`
   display: flex;
@@ -14,6 +15,16 @@ const GameStateItem = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
+`
+
+const StatLetter = styled.span.attrs(props => ({
+  className: `${!props.correct ? 'wrong' : ''}`
+}))`
+  color: hsl(90, 65%, 60%);
+  text-transform: capitalize;
+  &.wrong {
+    color: hsl(0, 65%, 60%);
+  }
 `
 
 export default function GameStats(props) {
@@ -30,6 +41,19 @@ export default function GameStats(props) {
       <GameStateItem>
         <span>Incorrect Guesses:</span>
         <span>{ wrongGuesses }</span>
+      </GameStateItem>
+      <GameStateItem>
+        <span>Guessed Letters</span>
+        <span>{
+          guessedLetters.map((letter, i) => {
+            const correct = word.indexOf(letter) > -1
+            return (
+              <span>
+                <StatLetter correct={correct}>{letter}</StatLetter> {i < guessedLetters.length - 1 ? ',' : ''}
+              </span>
+            )
+          })
+        }</span>
       </GameStateItem>
     </GameStatsWrapper>
   )
